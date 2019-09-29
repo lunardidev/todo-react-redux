fa561f0e5d4b69d1677322134d7da49ca1d9c3d2
Felipe <felipe@lunardi.dev>
2019-09-29 16:08:52 -0300

    build

---
+++ build/precache-manifest.1d6f7fb799c70a5ee93c9d6879f27d3f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1478a5766eef6411e726684e2fc3cf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3406eca8544669e3eb14",
+    "url": "/static/js/2.d1beebc3.chunk.js"
+  },
+  {
+    "revision": "4f18ab412dff8642ca7d",
+    "url": "/static/js/main.d0ad181a.chunk.js"
+  },
+  {
+    "revision": "ddb6388334b104fdd636",
+    "url": "/static/js/runtime-main.1a26f7ff.js"
+  }
+]);
